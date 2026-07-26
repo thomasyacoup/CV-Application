151e395b4b3823923d523f8b07aebb9a2adb5c61
thomas011001 <thomasyacoub321@gmail.com>
2026-07-26 23:51:04 +0300

    refactor: use the school instead of university

--- src/components/form/Option.jsx
+++ src/components/form/Option.jsx
@@ -2,7 +2,7 @@ function Option({ data, handleClick, handleDelete, display }) {
   return (
     <div className={`option ${display}`}>
       <button className="option-main" onClick={handleClick}>
-        {data.university || data.category || data.project || data.experience}
+        {data.school || data.category || data.project || data.experience}
       </button>
       <button className="option-delete" onClick={handleDelete}>
         Delete
