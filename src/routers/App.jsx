2c4687d96aa8ee65a2481b006f54173abb5fabb7
thomas011001 <thomasyacoub321@gmail.com>
2026-07-26 07:18:35 +0300

    refacor: delete the unused old states

--- src/routers/App.jsx
+++ src/routers/App.jsx
@@ -1,73 +1,13 @@
-import { useState } from "react";
-import "../App.css";
-import MyDocument from "../components/Cv";
 import Forms from "../components/Form";
+import { Resume } from "../components/Resume";
 
 function App() {
-  const [personalInfo, setPersonalInfo] = useState({
-    fullName: "Thomas Yacoub",
-    phoneNumber: "010945",
-    email: "thomas@gmail.com",
-    gitHubUrl: "https://github.com",
-  });
-
-  const [education, setEducation] = useState([
-    {
-      university: "auc",
-      field: "computer scince degree",
-      degree: "Certificate",
-      date: "May 2023",
-    },
-  ]);
-
-  const [experience, setExperience] = useState([
-    {
-      experience: "Frontend Developer Internship",
-      description: "Built responsive UI using React and CSS",
-    },
-  ]);
-
-  const [skills, setSkills] = useState([
-    {
-      category: "Languages & Dev Tools",
-      skills: "js html css",
-    },
-  ]);
-
-  const [project, setProject] = useState([
-    {
-      project: "My Portfolio",
-      description: "HTML, CSS, responsive design, and animations",
-    },
-  ]);
 
   return (
-    <>
-      <nav>
-        <span>By: Thomas Yacoub</span>
-        <span>
-          <a href="#">Github</a>
-        </span>
-      </nav>
-      <main>
-        <Forms
-          personalFormHandler={setPersonalInfo}
-          educationFormHandler={setEducation}
-          experienceFormHandler={setExperience}
-          skillsFormHandler={setSkills}
-          projectFormHandler={setProject}
-        />
-        <div className="panel">
-          <MyDocument
-            project={project}
-            skills={skills}
-            personalInfo={personalInfo}
-            education={education}
-            experience={experience}
-          />
-        </div>
+      <main className="flex h-dvh">
+        <Forms/>
+        <Resume />
       </main>
-    </>
   );
 }
 
