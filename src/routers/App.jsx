07b936a60799ab9ac390abd00c62f318f108ecfe
thomas011001 <thomasyacoub321@gmail.com>
2026-07-27 09:22:27 +0300

    fix: enhance layout for main component with print styles

--- src/routers/App.jsx
+++ src/routers/App.jsx
@@ -4,8 +4,8 @@ import { Resume } from "../components/Resume";
 function App() {
 
   return (
-      <main className="flex h-dvh">
-        <Forms/>
+      <main className="flex h-dvh relative print:h-auto! print:block! print:static!">
+        <Forms />
         <Resume />
       </main>
   );
