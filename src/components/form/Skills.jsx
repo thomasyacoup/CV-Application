bcf8830f70a3147385e9226979c2173e75639af6
thomas011001 <thomasyacoub321@gmail.com>
2026-07-27 06:34:55 +0300

    refactor: simplify SkillsInfo component by removing unused state and handlers

--- src/components/form/Skills.jsx
+++ src/components/form/Skills.jsx
@@ -1,53 +1,16 @@
 import { useState } from "react";
 import Option from "./Option";
+import { useResume } from "@/context/ResumeContext";
+import { DescriptionEditor } from "../ui/DescriptionEditor";
 
-function SkillsInfo({ onSubmit, onToggle, isActive }) {
-  const [data, setData] = useState([
-    {
-      category: "Languages & Dev Tools",
-      skills: "js html css",
-    },
-  ]);
+function SkillsInfo({ onToggle, isActive }) {
+  const { skills, setSkills } = useResume()
 
-  const [state, setState] = useState("normal");
-
-  const [category, setCategory] = useState("");
-  const [skills, setSkills] = useState("");
-
-  function onSave() {
-    onSubmit(data);
-  }
-
-  function handleOptionClick(index) {
-    setSkills(data[index].skills);
-    setCategory(data[index].category);
-    setState(index);
-  }
-
-  function handleOptionDelete(index) {
-    const updated = [...data];
-    updated[index].display = "none";
-    setData(updated);
-    setState("normal");
-  }
+  const [text, setText] = useState(skills)
 
   function handleFormSubmit(e) {
     e.preventDefault();
-    const newEd = { category, skills };
-    setData([...data, newEd]);
-    setCategory("");
-    setSkills("");
-    setState("normal");
-  }
-
-  function handleEditSubmit(e) {
-    e.preventDefault();
-    const updated = [...data];
-    updated[state] = { category, skills };
-    setData(updated);
-    setCategory("");
-    setSkills("");
-    setState("normal");
+    setSkills(text)
   }
 
   return (
@@ -65,102 +28,17 @@ function SkillsInfo({ onSubmit, onToggle, isActive }) {
         Skills
       </button>
       <div className="toggler-target">
-        {state == "normal" ? (
-          <>
-            <div className="options">
-              {data.map((item, index) => (
-                <Option
-                  display={item.display || "block"}
-                  key={index}
-                  handleClick={() => handleOptionClick(index)}
-                  handleDelete={() => handleOptionDelete(index)}
-                  data={item}
-                />
-              ))}
-            </div>
-            <div className="btns-flex">
-              <button onClick={() => setState("new")} className="save-btn">
-                New
-              </button>
-              <button className="save-btn" onClick={onSave}>
-                Save
-              </button>
-            </div>
-          </>
-        ) : state == "new" ? (
-          <form onSubmit={handleFormSubmit}>
+        <form onSubmit={handleFormSubmit}>
             <div className="form-flex">
-              <label htmlFor="">
-                Category
-                <input
-                  required
-                  type="text"
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value)}
-                />
-              </label>
-              <label htmlFor="">
+              <label>
                 Skills
-                <textarea
-                  required
-                  value={skills}
-                  onChange={(e) => setSkills(e.target.value)}
-                ></textarea>
+                <DescriptionEditor value={text} onChange={setText} />
               </label>
             </div>
             <div className="btns-flex">
-              <button
-                type="button"
-                onClick={() => {
-                  setState("normal");
-                  setCategory("");
-                  setSkills("");
-                }}
-                className="save-btn"
-              >
-                Cansle
-              </button>
               <input type="submit" value="add" className="save-btn" />
             </div>
           </form>
-        ) : (
-          <form onSubmit={handleEditSubmit}>
-            <div className="form-flex">
-              <label htmlFor="">
-                Category
-                <input
-                  required
-                  type="text"
-                  value={category}
-                  onChange={(e) => setCategory(e.target.value)}
-                />
-              </label>
-              <label htmlFor="">
-                Field of Study
-                <textarea
-                  required
-                  type="text"
-                  value={skills}
-                  onChange={(e) => setSkills(e.target.value)}
-                ></textarea>
-              </label>
-            </div>
-            <div className="btns-flex">
-              <button
-                className="save-btn"
-                type="button"
-                onClick={() => {
-                  setState("normal");
-                  setCategory("");
-                  setSkills("");
-                }}
-              >
-                Cancel
-              </button>
-              <input type="submit" value="Save" className="save-btn" />
-            </div>
-          </form>
-        )}
       </div>
     </div>
   );
