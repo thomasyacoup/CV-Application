76c87c396e8b63c793a5803aa40d5e6187a935d8
thomas011001 <thomasyacoub321@gmail.com>
2026-07-27 03:01:34 +0300

    refactor: use use jobTitle insted of experience

--- src/components/form/Option.jsx
+++ src/components/form/Option.jsx
@@ -2,7 +2,7 @@ function Option({ data, handleClick, handleDelete, display }) {
   return (
     <div className={`option ${display}`}>
       <button className="option-main" onClick={handleClick}>
-        {data.school || data.category || data.project || data.experience}
+        {data.school || data.category || data.project || data.jobTitle}
       </button>
       <button className="option-delete" onClick={handleDelete}>
         Delete
