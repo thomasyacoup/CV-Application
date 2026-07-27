aa9485c6214addb71882114a897c30f6e0390706
thomas011001 <thomasyacoub321@gmail.com>
2026-07-27 09:20:42 +0300

    refactor: simplify Forms component by removing unused form handlers and enhancing layout

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,47 +5,45 @@ import { useState } from "react";
 import SkillsInfo from "./form/Skills";
 import ProjectsInfo from "./form/Projects";
 import ExperienceInfo from "./form/Experience";
+import { Button } from "./ui/button";
 
-function Forms({
-  personalFormHandler,
-  educationFormHandler,
-  experienceFormHandler,
-  projectFormHandler,
-  skillsFormHandler,
-}) {
+function Forms() {
   const [toggle, setToggle] = useState("None");
 
+
   return (
-    <div className="form">
+    <div className="form h-dvh flex-1 ">
       <PersonalInfo
-        onSubmit={personalFormHandler}
         isActive={toggle === "personal"}
         onToggle={setToggle}
       />
       <hr />
       <EducationInfo
-        onSubmit={educationFormHandler}
         isActive={toggle === "education"}
         onToggle={setToggle}
       />
       <hr />
       <ExperienceInfo
-        onSubmit={experienceFormHandler}
         isActive={toggle === "experience"}
         onToggle={setToggle}
       />
       <hr />
       <ProjectsInfo
-        onSubmit={projectFormHandler}
         isActive={toggle === "project"}
         onToggle={setToggle}
       />
       <hr />
       <SkillsInfo
-        onSubmit={skillsFormHandler}
         isActive={toggle === "skills"}
         onToggle={setToggle}
       />
+
+      <Button
+        onClick={() => window.print()}
+      >
+        Download PDF
+      </Button>
+      
     </div>
   );
 }
