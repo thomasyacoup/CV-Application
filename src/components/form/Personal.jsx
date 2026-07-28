41b62d0733d9f7db82046afbc8b0e6dfd33b810f
thomas011001 <thomasyacoub321@gmail.com>
2026-07-28 09:19:03 +0300

    feat: enhance PersonalInfo component with collapsible UI and improved form structure

--- src/components/form/Personal.jsx
+++ src/components/form/Personal.jsx
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { useResume } from "@/context/ResumeContext.jsx";
+import { CollapseButtton } from "../ui/CollapseButton";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Field, FieldGroup, FieldLabel } from "../ui/field";
+import { CollapseContainer } from "../ui/CollapseContainer";
 
 function PersonalInfo({ onToggle, isActive }) {
   const resumeData = useResume();
@@ -12,77 +17,73 @@ function PersonalInfo({ onToggle, isActive }) {
 
   function handleSubmit(e) {
     e.preventDefault();
+    console.log("hi")
     const data = { name, phone, email, github, linkedin };
 
     resumeData.setPersonalInfo(data);
   }
 
   return (
-    <div className={isActive ? "active category" : "category"}>
-      <button
-        className="toggler"
-        onClick={() => {
-          if (isActive) {
-            onToggle("none");
-          } else {
-            onToggle("personal");
-          }
-        }}
-      >
-        Personal Info
-      </button>
-      <form onSubmit={handleSubmit} className="toggler-target">
-        <div className="form-flex">
-          <label>
-            Full Name
-            <input
-              required
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-            />
-          </label>
-          <label htmlFor="">
-            Phone
-            <input
-              required
-              type="text"
-              onChange={(e) => setPhone(e.target.value)}
-              value={phone}
-            />
-          </label>
-          <label>
-            Email
-            <input
-              required
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-            />
-          </label>
-          <label>
-            Github Url
-            <input
-              required
-              type="url"
-              onChange={(e) => setGithub(e.target.value)}
-              value={github}
-            />
-          </label>
-          <label>
-            LinkedIn Url
-            <input
-              required
-              type="url"
-              onChange={(e) => setLinkedin(e.target.value)}
-              value={linkedin}
-            />
-          </label>
-        </div>
-        <div className="btns-flex">
-          <input type="submit" value="save" className="save-btn" />
-        </div>
-      </form>
+    <div>
+      <CollapseButtton isActive={isActive} onToggle={onToggle} sectionName="personal">
+        Personal Information
+      </CollapseButtton>
+      <CollapseContainer isActive={isActive}>
+        <form onSubmit={handleSubmit}>
+          <FieldGroup className={"grid grid-cols-2 gap-4 p-4"}>
+            <Field>
+              <FieldLabel>
+                Full Name 
+              </FieldLabel>
+                <Input
+                  required
+                  type="text"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                />
+            </Field>
+            <Field>
+            <FieldLabel>Phone Number</FieldLabel>
+              <Input
+                required
+                type="text"
+                onChange={(e) => setPhone(e.target.value)}
+                value={phone}
+              />
+            </Field>
+            <Field className={"col-span-2"}>
+              <FieldLabel>Email</FieldLabel>
+              <Input
+                required
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </Field>
+            <Field className={"col-span-2"}>
+              <FieldLabel>Github Url</FieldLabel>
+              <Input
+                required
+                type="url"
+                onChange={(e) => setGithub(e.target.value)}
+                value={github}
+              />
+            </Field>
+            <Field className={"col-span-2"}>
+              <FieldLabel>LinkedIn Url</FieldLabel>
+              <Input
+                required
+                type="url"
+                onChange={(e) => setLinkedin(e.target.value)}
+                value={linkedin}
+              />
+            </Field>
+          </FieldGroup>
+          <div className="flex gap-4 justify-end p-4">
+            <Button type="submit">Save</Button>
+          </div>
+        </form>
+      </CollapseContainer>
     </div>
   );
 }
