2a243af3d1fc9a23f4e5b4fca7c2025f7c693855
thomas011001 <thomasyacoub321@gmail.com>
2026-07-28 09:16:59 +0300

    refactor: update Option component structure and styling for consistency

--- src/components/form/Option.jsx
+++ src/components/form/Option.jsx
@@ -1,12 +1,14 @@
-function Option({ data, handleClick, handleDelete, display }) {
+import { Button } from "../ui/button";
+
+function Option({ data, handleClick, handleDelete }) {
   return (
-    <div className={`option ${display}`}>
-      <button className="option-main" onClick={handleClick}>
+    <div className="flex justify-between items-center border-b border-b-border p-2 gap-2">
+      <button onClick={handleClick} className="flex-1 text-left border-border border-r hover:underline hover:cursor-pointer">
         {data.school || data.category || data.title || data.jobTitle}
       </button>
-      <button className="option-delete" onClick={handleDelete}>
+      <Button variant="destructive" onClick={handleDelete}>
         Delete
-      </button>
+      </Button>
     </div>
   );
 }
