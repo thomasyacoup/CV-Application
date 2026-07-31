f54d0a0f198e3731a7bccbc128a62b39a45bb895
thomas011001 <thomasyacoub321@gmail.com>
2026-07-31 06:36:16 +0300

    refactor: update App component layout to use grid and include Header component

--- src/routers/App.jsx
+++ src/routers/App.jsx
@@ -1,10 +1,12 @@
 import Forms from "../components/Form";
 import { Resume } from "../components/Resume";
+import { Header } from "@/components/Header";
 
 function App() {
 
   return (
-      <main className="flex h-dvh relative print:h-auto! print:block! print:static!">
+      <main className="grid grid-cols-2 h-dvh relative print:h-[297mm]! print:block! print:static! p-2 gap-2 bg-[#dddddd] print:p-0">
+        <Header />
         <Forms />
         <Resume />
       </main>
