d4cbc213c7f0adba7eaf4abc1ab875d42b8a281d
thomas011001 <thomasyacoub321@gmail.com>
2026-08-02 05:23:26 +0300

    refactor: extract Item component from ItemsView for better readability and maintainability

--- src/components/ui/ItemsView.jsx
+++ src/components/ui/ItemsView.jsx
@@ -1,10 +1,39 @@
 import { Reorder, useDragControls } from "motion/react";
-import Option from "../form/Option";
 import { Button } from "./button";
+import { GripVertical } from "lucide-react";
 
-export function ItemsView({items, dispatchItems, setState, handleOptionClick, handleOptionDelete}) {
+function Item({item, handleClick, handleDelete}) {
   const controls = useDragControls()
   
+  return (
+    <Reorder.Item
+      dragListener={false}
+      dragControls={controls}
+      value={item}
+    >
+      <div className="bg-white flex justify-between items-center border-b border-border">
+        <div 
+          className="cursor-grab p-2"
+          onPointerDown={(e) => { e.preventDefault(); controls.start(e) }}
+        >
+          <GripVertical className="text-gray-400 font size-4" />
+        </div>
+        <div onClick={handleClick} role="button" className="p-2 flex-1 border-border border-x group hover:cursor-pointer">
+          <button className="text-left group-hover:underline">
+            {item.school || item.category || item.title || item.jobTitle}
+          </button>
+        </div>
+        <div className="p-2">
+          <Button variant="destructive" onClick={handleDelete}>
+            Delete
+          </Button> 
+        </div>
+      </div>
+    </Reorder.Item>
+  )
+}
+
+export function ItemsView({items, dispatchItems, setState, handleOptionClick, handleOptionDelete}) {
   return (
     <div className="flex flex-col">
       <Reorder.Group
@@ -14,20 +43,12 @@ export function ItemsView({items, dispatchItems, setState, handleOptionClick, ha
         onReorder={(newOrder) => dispatchItems({type: "REORDER", payload: newOrder})}
       >
         {items.map(item => (
-        <Reorder.Item
-          dragListener={false}
-          dragControls={controls}
-          value={item}
-          key={item.id}
-        >
-          <Option
-            controls={controls}
-            display={item.display || "block"}
-            handleClick={() => handleOptionClick(item.id)}
+          <Item 
+            item={item} 
+            handleClick={() => handleOptionClick(item.id)} 
             handleDelete={() => handleOptionDelete(item.id)}
-            data={item}
+            key={item.id}
           />
-        </Reorder.Item>
         ))}
       </Reorder.Group>
       <div className="p-4">
