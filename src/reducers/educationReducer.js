8d3e5fb52dbf0dff63912ab6e5f0e8f6079e8fdf
thomas011001 <thomasyacoub321@gmail.com>
2026-08-02 04:35:43 +0300

    fix: add REORDER case to publicReducer for handling item reordering

--- src/reducers/educationReducer.js
+++ src/reducers/educationReducer.js
@@ -3,6 +3,7 @@ export function publicReducer(state, action) {
     case "ADD": return [...state, action.payload]; 
     case "UPDATE": return state.map(item => action.payload.id ==  item.id ? action.payload : item);
     case "REMOVE": return state.filter(item => item.id != action.payload.id);
+    case "REORDER": return action.payload;
   }
 }
 
