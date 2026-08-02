13c622040c84bac081ac291f9fa40e2c6540b925
thomas011001 <thomasyacoub321@gmail.com>
2026-08-02 03:23:58 +0300

    fix: update Option component layout for improved structure and styling

--- src/components/form/Option.jsx
+++ src/components/form/Option.jsx
@@ -1,14 +1,22 @@
+import { GripVertical } from "lucide-react";
 import { Button } from "../ui/button";
 
 function Option({ data, handleClick, handleDelete }) {
   return (
-    <div className="flex justify-between items-center border-b border-b-border p-2 gap-2">
-      <button onClick={handleClick} className="flex-1 text-left border-border border-r hover:underline hover:cursor-pointer">
-        {data.school || data.category || data.title || data.jobTitle}
-      </button>
-      <Button variant="destructive" onClick={handleDelete}>
-        Delete
-      </Button>
+    <div className="bg-white flex justify-between items-center border-b border-border">
+      <div className="cursor-grab p-2">
+        <GripVertical className="text-gray-400 font size-4" />
+      </div>
+      <div onClick={handleClick} role="button" className="p-2 flex-1 border-border border-x group hover:cursor-pointer">
+        <button className="text-left group-hover:underline">
+          {data.school || data.category || data.title || data.jobTitle}
+        </button>
+      </div>
+      <div className="p-2">
+        <Button variant="destructive" onClick={handleDelete}>
+          Delete
+        </Button> 
+      </div>
     </div>
   );
 }
