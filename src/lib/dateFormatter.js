1845de639d77aab1c730db1665a78968f4c47a2f
thomas011001 <thomasyacoub321@gmail.com>
2026-08-06 23:02:58 +0300

    feat: add handling for "Present" date string in dateFormatter function

--- src/lib/dateFormatter.js
+++ src/lib/dateFormatter.js
@@ -1,4 +1,6 @@
 export function dateFormatter(dateString) {
+  if (dateString == "Present") return dateString;
+  
   const [year, month] = dateString.split('-');
   const date = new Date(year, month - 1);
   
